d50112d81aa4ee0adb1f6a2f635a19fdde7c0197
ibrahim29asad <ibrahim.asad2019@gmail.com>
2025-06-06 00:27:24 -0600

    Created and Started FrontEnd

---
+++ Student_FrontEnd/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="MainBox">
+      <h3>
+        Simple Student Managment System
+      </h3>
+      <div className='SearchBox'>
+        <h3>
+          SkYeet
+        </h3>
+      </div>
+      <div className='DisplayBox'>
+        <h3>
+          Here are the Results sir
+        </h3>
+      </div>
+
+     </div>
+    </>
+  )
+}
+
+export default App

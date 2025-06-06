33878384e69cc7efdca6476b2ddfcad56a9df5cb
ibrahim29asad <ibrahim.asad2019@gmail.com>
2025-06-06 15:41:06 -0600

    finished the Setup and Creation of the basic Front End

--- Student_FrontEnd/src/App.jsx
+++ Student_FrontEnd/src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
+
 import './App.css'
 
 function App() {
   
-
+  const [isModalOpen, setisModalOpen] = useState(false);
+  const AddPopup = ()=>{
+    setisModalOpen(true);
+  }
+  const ClosePopup = ()=>{
+    setisModalOpen(false);
+  }
   return (
     <>
      <div className="MainBox">
@@ -10,14 +18,64 @@ function App() {
         Simple Student Managment System
       </h3>
       <div className='SearchBox'>
-        <h3>
-          SkYeet
-        </h3>
+          
+          <input className = "search-input" type="search" name="searchinput" placeholder="Student ID" />
+          <button className='addBtn' onClick={AddPopup}> Add</button>
+          
       </div>
       <div className='DisplayBox'>
-        <h3>
-          Here are the Results sir
-        </h3>
+        {isModalOpen && (
+  <div className="AddModalOverlay">
+    <div className="AddModalPopup">
+      <div className="AddModalPopupContent">Student Information</div>
+      <div className="AddModalPopupContent">
+        <input className="search-input" type="text" name="InputName" placeholder="Name" />
+      </div>
+      <div className="AddModalPopupContent">
+        <input className="search-input" type="text" name="InputMajor" placeholder="Major" />
+      </div>
+      <div className="AddModalPopupContent">
+        <input className="search-input" type="text" name="InputEmail" placeholder="Email" />
+      </div>
+      <div>
+        <button className="Addbtn">Add</button>
+        <button className="closebtn" onClick={ClosePopup}>Close</button>
+      </div>
+    </div>
+  </div>
+)}
+
+
+        <table className='studentTable'>
+          <tr>
+            <th>studentID</th>
+          <th>Name</th>
+          <th>Major</th>
+          <th>Email</th>
+          <th>Edit</th>
+          <th>Delete</th>
+          </tr>
+          <tbody>
+            <tr>
+              <td>16</td>
+              <td>Steve</td>
+              <td>P.E.</td>
+              <td>steve.pe@schoolsEmail.com</td>
+              <td><button className='editBtn'> Edit</button></td>
+              <td><button className='delBtn'> Del</button></td>
+
+            </tr>
+            <tr>
+              <td>17</td>
+              <td>Yanis</td>
+              <td>Biology</td>
+              <td>Yanise.bio@schoolsEmail.com</td>
+              <td><button className='editBtn'> Edit</button></td>
+              <td><button className='delBtn'> Del</button></td>
+
+            </tr>
+          </tbody>
+        </table>
       </div>
 
      </div>

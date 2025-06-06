2104df99cf45c404d6966c8f6dde0763c1d93002
ibrahim29asad <ibrahim.asad2019@gmail.com>
2025-06-06 17:15:36 -0600

    Implemented GET/ADD/UPDATE
    Next wokring on delete and validation/error handle

--- Student_FrontEnd/src/App.jsx
+++ Student_FrontEnd/src/App.jsx
@@ -1,16 +1,73 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import './App.css'
 
 function App() {
   
+  const [Students, setStudents] = useState([]);
+  const [filterStudents, setfilterStudents] = useState([]);
   const [isModalOpen, setisModalOpen] = useState(false);
+  const [StudentData, setStudentData] = useState({name:"",major:"",email:""});
+
   const AddPopup = ()=>{
     setisModalOpen(true);
   }
   const ClosePopup = ()=>{
     setisModalOpen(false);
+    setStudentData({ name: "", major: "", email: "" });
   }
+  const getAllStudents = () => {
+    axios.get("http://localhost:3000/students").then((res) =>{
+      setStudents(res.data)
+      setfilterStudents(res.data);
+    })
+   
+  }
+  
+  useEffect(()=>{getAllStudents()}, [])
+
+  const ObtainStudents = (e) => {
+    const SearchValue = e.target.value.toLowerCase();
+    const filterterdData = Students.filter(student=>student.name.toLowerCase().includes(SearchValue) ||
+                    student.major.toLowerCase().includes(SearchValue) ||
+                    student.email.toLowerCase().includes(SearchValue));
+    setfilterStudents(filterterdData);
+  }
+
+  const HandleStudentData = (e) => {
+    setStudentData({...StudentData, [e.target.name]: e.target.value});
+  }
+  const HandleUpdate = (student) => {
+    setStudentData(student);
+    setisModalOpen(true);
+    
+  }
+
+  const AddingStudents = async(e) => {
+    // Stopping it from reloading until I am done
+    e.preventDefault();
+    if(StudentData.studentid){
+      await axios.patch(`http://localhost:3000/students/${StudentData.studentid}`, StudentData).then((res) =>{
+      console.log(res.data);
+      setisModalOpen(false);
+      getAllStudents();
+    })
+    }
+    else{
+    await axios.post("http://localhost:3000/students", StudentData).then((res) =>{
+      console.log(res.data);
+      setisModalOpen(false);
+      getAllStudents();
+    })
+    }
+
+
+    
+    
+  }
+
+
   return (
     <>
      <div className="MainBox">
@@ -19,7 +76,7 @@ function App() {
       </h3>
       <div className='SearchBox'>
           
-          <input className = "search-input" type="search" name="searchinput" placeholder="Student ID" />
+          <input className = "search-input" onChange={ObtainStudents} type="search" name="searchinput" placeholder="Search Student" />
           <button className='addBtn' onClick={AddPopup}> Add</button>
           
       </div>
@@ -29,16 +86,16 @@ function App() {
     <div className="AddModalPopup">
       <div className="AddModalPopupContent">Student Information</div>
       <div className="AddModalPopupContent">
-        <input className="search-input" type="text" name="InputName" placeholder="Name" />
+        <input className="search-input" type="text" value={StudentData.name} onChange={HandleStudentData} name="name" id ="Name" placeholder="Name" />
       </div>
       <div className="AddModalPopupContent">
-        <input className="search-input" type="text" name="InputMajor" placeholder="Major" />
+        <input className="search-input" type="text" value={StudentData.major} onChange={HandleStudentData} name="major" id ="Major" placeholder="Major" />
       </div>
       <div className="AddModalPopupContent">
-        <input className="search-input" type="text" name="InputEmail" placeholder="Email" />
+        <input className="search-input" type="text" value={StudentData.email} onChange={HandleStudentData} name="email" id ="Email" placeholder="Email" />
       </div>
       <div>
-        <button className="Addbtn">Add</button>
+        <button className="Addbtn" onClick={AddingStudents} >{StudentData.studentid? "Update" : "Add"}</button>
         <button className="closebtn" onClick={ClosePopup}>Close</button>
       </div>
     </div>
@@ -56,7 +113,30 @@ function App() {
           <th>Delete</th>
           </tr>
           <tbody>
-            <tr>
+            {Students && filterStudents.map(student => {
+              return (<tr key={student.studentid}>
+                <td>{student.studentid}</td>
+                <td>{student.name}</td>
+                <td>{student.major}</td>
+                <td>{student.email}</td>
+                <td><button className='editBtn' onClick={() => HandleUpdate(student)}> Edit</button></td>
+                <td><button className='delBtn'> Del</button></td>
+              </tr>)
+
+            })}
+
+          </tbody>
+        </table>
+      </div>
+
+     </div>
+    </>
+  )
+}
+
+export default App
+
+            /* <tr>
               <td>16</td>
               <td>Steve</td>
               <td>P.E.</td>
@@ -72,15 +152,4 @@ function App() {
               <td>Yanise.bio@schoolsEmail.com</td>
               <td><button className='editBtn'> Edit</button></td>
               <td><button className='delBtn'> Del</button></td>
-
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-     </div>
-    </>
-  )
-}
-
-export default App
+            </tr> */
